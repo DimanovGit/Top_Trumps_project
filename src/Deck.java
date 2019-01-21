5981ff97a5ea1ee6d15285a51e132932e32fe888
DimanovGit <ivaylo_dimanov@abv.bg>
2019-01-21 23:22:48 +0000

    test

--- src/Deck.java
+++ src/Deck.java
@@ -4,21 +4,17 @@ public class Deck {
 	public Object cards;
 
 	public Deck(int i) {
-		// TODO Auto-generated constructor stub
 	}
 
 	public void shuffle() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	public Card dealCard() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public void returnCard(Card c) {
-		// TODO Auto-generated method stub
 		
 	}
 
