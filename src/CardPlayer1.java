464cbe1815be627c03289bbbf54b6caec1492d65
DimanovGit <ivaylo_dimanov@abv.bg>
2019-01-27 16:12:02 +0000

    CardPlayer edit

--- src/CardPlayer1.java
+++ src/CardPlayer1.java
@@ -5,9 +5,11 @@ public class CardPlayer1 {
 	
 	private List<Card> deck = new ArrayList<Card>();
 	private String name;
+	private boolean hasLost;
 	
 	public CardPlayer1(String name) {
 		this.name = name;
+		this.hasLost = false;
 	}
 
 	public void addCard(Card card) {
@@ -16,6 +18,22 @@ public class CardPlayer1 {
 	
 	public List<Card> getDeck() {
 		return deck;
+	}
+	
+	public Card getFirstCard() {
+		return deck.get(0);
+	}
+	
+	public void removeFirstCard() {
+		deck.remove(0);
+	}
+	
+	public boolean hasLost() {
+		return hasLost;
+	}
+	
+	public void hasLost(boolean hasLost) {
+		this.hasLost = hasLost;
 	}
 	
 	public String getName() {
