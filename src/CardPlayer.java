ec42a42a740b621ab61684751118472612761726
Ivaylo <2083549@student.gla.ac.uk>
2019-02-17 15:21:03 +0000

    CL start menu implemented

--- src/CardPlayer.java
+++ src/CardPlayer.java
@@ -1,5 +1,8 @@
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -73,4 +76,36 @@ public class CardPlayer {
 		return name;
 	}
 	
+	BufferedWriter writer;
+
+	public void writeStats(String line)
+	{
+	    try
+	    {
+	        File file = new File("GameStats.txt");
+	        file.createNewFile();
+
+	        writer = new BufferedWriter(new FileWriter(file));
+
+	        writer.write("Number of draws: " + drawnGames );
+	        writer.newLine();
+	        writer.write("Number of sentences: "   );
+	        writer.newLine();
+	        writer.write("Shortest sentence: " +  " words");
+	        writer.newLine();
+	        writer.write("Longest sentence: " +  " words");
+	        writer.newLine();
+	        writer.write("Average sentence: " +   " words");    
+	    }
+	    catch(FileNotFoundException e)
+	    {
+	        System.out.println("File Not Found");
+	        System.exit( 1 );
+	    }
+	    catch(IOException e)
+	    {
+	        System.out.println("something messed up");
+	        System.exit( 1 );
+	    }
+	}
 }
